30af1c375c00f99143e9df408f441a91afa1c26b
cicyyzx <cicyy.marical@gmail.com>
2025-04-02 11:31:53 +0000

    vbvbvbv

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     aumentarFontebotao.addEventListener('click', function(){
         tamanhoAtualFonte +=0,1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem'
+   
     })
-)}
-     const diminuirFontebotao = document.getElementById('diminuir-fonte');
 
 
     diminuirFontebotao.addEventListener('click', function(){
-        tamanhoAtualFonte +=0,1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}
+        tamanhoAtualFonte -=0,1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem'
     })
-)}
+})
